e1504660bd33a224d079a2825f8850fb1a3e36c7
Theo Lanman <theo.lanman@gmail.com>
2024-03-03 15:27:31 -0500

    finding model params for problem 1

--- experimentloop.py
+++ experimentloop.py
@@ -3,13 +3,13 @@
 # Preprocess data
 import os
 
-import pandas as pd
-
 import numpy as np
-
+import pandas as pd
+from sklearn.dummy import DummyClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.pipeline import FunctionTransformer, Pipeline
 
 # from sklearn.dummy import DummyClassifier
 
@@ -25,53 +25,76 @@ def load_data(data_path=DATA_PATH):
     return x_train_NC, y_train_N.squeeze()
 
 
-# add fxn contract
-def featurize(x_NC):
+def pipeline():
+    text_col_extractor = FunctionTransformer(lambda x_NC: x_NC.loc[:, "text"])
     vectorizer = CountVectorizer()
-    corpus = x_NC.loc[:, "text"]
-    # n rows, v features (vocab)
-    return vectorizer.fit_transform(corpus)  # may want to add vectorizer later
+    classifier = LogisticRegression(max_iter=400)
+    return Pipeline(
+        [
+            ("extract_text", text_col_extractor),
+            ("featurize", vectorizer),
+            ("classify", classifier),
+        ]
+    )
 
 
-# overview_data(x_train_nf, y_train_n)
-def cross_validation_auroc(x_NF, y_N):
-    model = LogisticRegression()
+def grid_search(x_NC, y_N, param_grid):
+    grid = GridSearchCV(pipeline(), param_grid=param_grid, scoring="roc_auc", n_jobs=-1)
 
-    # produce auroc scores by 5-fold cross-validation on (x_NF,y_N)
-    scores = cross_validate(model, x_NF, y_N, cv=5, scoring="roc_auc")
-
-    return np.mean(scores["test_score"])
+    grid.fit(x_NC, y_N)
+    return grid
 
 
-x_train_NC, y_train_N = load_data()
-features_NF = featurize(x_train_NC)
-avg_cv_scores = cross_validation_auroc(features_NF, y_train_N)
-print("avg:", np.round(avg_cv_scores, 3))
+def round_if_float(f, places=3):
+    if isinstance(f, float):
+        return np.round(f, places)
+    return f
 
 
-# model.fit(input_vectors_nv, y_train_n)
-# baseline_test_scores = cross_validate(
-#    DummyClassifier(strategy="constant", constant=0),
-#    input_vectors_nv,
-#    y_train_n,
-#    cv=5,
-#    scoring="roc_auc",
-# )
-# average_baseline_cv_scores = np.mean(baseline_test_scores["test_score"])
-# print("baseline: ", average_baseline_cv_scores)
-
-# Cross validation
-# def cross_validate(model): ...
+def round_dict_vals(d):
+    return {k: round_if_float(v) for k, v in d.items()}
 
 
-# split into folds
+def output_grid_result(name, grid):
+    print(name)
+    print("best score: ", np.round(grid.best_score_, 3))
+    print("best params: ", round_dict_vals(grid.best_params_))
+    print(
+        "vocab size: ",
+        len(grid.best_estimator_.named_steps["featurize"].vocabulary_),
+    )
+    best_model = grid.best_estimator_.named_steps["classify"]
+    print(best_model)
 
-# for each fold, fit and evaluate
+
+C_GRID = np.logspace(-9, 6, 31)
 
 
-# Call CV function
-# auroc = cross_validate(model)
+def main():
+    x_train_NC, y_train_N = load_data()
+    param_grids = {
+        "baseline": {"classify": [DummyClassifier(strategy="constant", constant=0)]},
+        "count_default": {
+            "featurize": [CountVectorizer()],
+            "classify__C": C_GRID,
+        },
+        "count_top_2000_words": {
+            "featurize": [CountVectorizer(max_features=2000)],
+            "classify__C": C_GRID,
+        },
+        "tfidf_default": {
+            "featurize": [TfidfVectorizer()],
+            "classify__C": C_GRID,
+        },
+    }
+    for name, grid in param_grids.items():
+        grid = grid_search(x_train_NC, y_train_N, param_grid=grid)
+        print("\n\n")
+        output_grid_result(name, grid)
 
+
+if __name__ == "__main__":
+    main()
 
 # TODO Refine with hyperparameters
 
